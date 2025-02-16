0ab6e19e84e57c5e8f822193b4e8eeb63d5f8d03
Andrei Maslov <andd.maslov@gmail.com>
2025-02-16 03:47:29 +0300

    Начал объявлять константы

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+const (
+	tableBeginRow       string = "23"
+	tableEndRow         string = "76"
+	jobCostColumn       string = "E"
+	employeeBeginColumn string = "F"
+)
+
+var excludeRows = []string{
+	"52",
+	"53",
+	"76",
+}
+
 func main() {
 	fmt.Println("RitaulPay запущен!")
 }
