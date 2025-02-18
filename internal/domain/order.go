a8561168d37030825e6daf817c4626abd2f4ef6d
Andrei Maslov <andd.maslov@gmail.com>
2025-02-18 05:39:14 +0300

    добавил новые поля для описания заказа (информация о заказчике, информация об умершем)

--- internal/domain/order.go
+++ internal/domain/order.go
@@ -14,5 +14,18 @@ type Service struct {
 
 // Заказ
 type Order struct {
+	OrderNumber      string
+	CustomerFullName string
+	HomePhome        string
+	MobilePhone      string
+	Address          string
+
+	DeceasedFullName     string
+	DeceasedAge          int8
+	DeceasedHeight       string // тип string на случай если в excel файле запишут "1м 65см"
+	DeceasedClothingSize string
+	DeceasedBirthDate    string // тип так же string так как не знаю в каком формате будут писать дату
+	DeceasedDeathDate    string // аналогично :point_up:
+
 	Services []Service
 }
