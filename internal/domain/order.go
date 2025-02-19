2407e07d44fe88bdce2105c6bbc9fb3edda832da
Andrei Maslov <andd.maslov@gmail.com>
2025-02-19 09:40:35 +0300

    обновил доменную структуру order под обновленный файл excel

--- internal/domain/order.go
+++ internal/domain/order.go
@@ -1,5 +1,10 @@
 package domain
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Исполнитель работы / услуги
 type Preformer struct {
 	Name string
@@ -16,16 +21,37 @@ type Service struct {
 type Order struct {
 	OrderNumber      string
 	CustomerFullName string
-	HomePhome        string
+	HomePhone        string
 	MobilePhone      string
 	Address          string
 
 	DeceasedFullName     string
-	DeceasedAge          int8
+	DeceasedAge          string
 	DeceasedHeight       string // тип string на случай если в excel файле запишут "1м 65см"
 	DeceasedClothingSize string
 	DeceasedBirthDate    string // тип так же string так как не знаю в каком формате будут писать дату
 	DeceasedDeathDate    string // аналогично :point_up:
 
 	Services []Service
+}
+
+func (o Order) String() string {
+	var sb strings.Builder
+	sb.WriteString("Order Details:\n")
+
+	sb.WriteString(fmt.Sprintf("├─ Номер счета:      %s\n", o.OrderNumber))
+	sb.WriteString(fmt.Sprintf("├─ Заказчик:         %s\n", o.CustomerFullName))
+	sb.WriteString(fmt.Sprintf("├─ Контакты:         %s / %s\n", o.HomePhone, o.MobilePhone))
+	sb.WriteString(fmt.Sprintf("├─ Адрес:            %s\n", o.Address))
+	sb.WriteString(fmt.Sprintf("├─ Умерший:          %s\n", o.DeceasedFullName))
+	sb.WriteString(fmt.Sprintf("├─ Возраст/Рост:     %s лет / %s см\n", o.DeceasedAge, o.DeceasedHeight))
+	sb.WriteString(fmt.Sprintf("├─ Размер одежды:    %s\n", o.DeceasedClothingSize))
+	sb.WriteString(fmt.Sprintf("├─ Даты:             %s - %s\n", o.DeceasedBirthDate, o.DeceasedDeathDate))
+	sb.WriteString(fmt.Sprintf("└─ Услуги (%d):\n", len(o.Services)))
+
+	for i, s := range o.Services {
+		sb.WriteString(fmt.Sprintf("   %d. %s (%.2f руб)\n", i+1, s.Name, s.Cost))
+	}
+
+	return sb.String()
 }
