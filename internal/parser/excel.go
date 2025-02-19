1a8b19b7da1326e0c0c31e7360544f522d9220a6
Andrei Maslov <andd.maslov@gmail.com>
2025-02-19 09:41:16 +0300

    Ревью кода парсера

--- internal/parser/excel.go
+++ internal/parser/excel.go
@@ -14,15 +14,15 @@ const (
 	OrderNumberCellAdrs      = "AO13"
 	CustomerFullNameCellAdrs = "O16"
 	HomePhomeCellAdrs        = "R17"
-	MobilePhoneCellAdrs      = "AW17"
+	MobilePhoneCellAdrs      = "AY17"
 	AddressCellAdrs          = "H18"
 
 	DeceasedFullNameCellAdrs     = "O19"
 	DeceasedAgeCellAdrs          = "I20"
-	DeceasedHeightCellAdrs       = "AR20"
-	DeceasedClothingSizeCellAdrs = "AR20" // Такой же адрес как и у роста
-	DeceasedBirthDateCellAdrs    = "W21"
-	DeceasedDeathDateCellAdrs    = "W21" // TODO разделить в excel шаблоне эти поля
+	DeceasedHeightCellAdrs       = "Z20"
+	DeceasedClothingSizeCellAdrs = "BA20"
+	DeceasedBirthDateCellAdrs    = "N21"
+	DeceasedDeathDateCellAdrs    = "AY21"
 )
 
 func Parse(filepath string) (*domain.Order, error) {
@@ -36,6 +36,11 @@ func Parse(filepath string) (*domain.Order, error) {
 	order := domain.Order{}
 
 	err = parseOrderInfo(f, &order)
+	if err != nil {
+		// TODO Ругнуться
+	}
+
+	// err := parseServices(f, &order)
 
 	return &order, err
 }
@@ -46,95 +51,31 @@ func parseOrderInfo(f *excelize.File, order *domain.Order) error {
 		return nil // TODO написать свою ошибку входного аргумента
 	}
 
-	orderNumber, err := f.GetCellValue(OrderSheet, OrderNumberCellAdrs)
-	if err != nil {
-		fmt.Println("ERR: Не удалось получить значение номера счета заказа")
-		return err
+	cells := map[string]struct {
+		cellAddress string
+		field       *string
+		title       string
+	}{
+		"order_number":           {OrderNumberCellAdrs, &order.OrderNumber, "Номер счет-заказа"},
+		"customer_full_name":     {CustomerFullNameCellAdrs, &order.CustomerFullName, "ФИО заказчика"},
+		"home_phone":             {HomePhomeCellAdrs, &order.HomePhone, "Добашний телефон"},
+		"mobile_phone":           {MobilePhoneCellAdrs, &order.MobilePhone, "Мобильный телефон"},
+		"address":                {AddressCellAdrs, &order.Address, "Адрес"},
+		"deceased_full_name":     {DeceasedFullNameCellAdrs, &order.DeceasedFullName, "ФИО умершего"},
+		"deceased_age":           {DeceasedAgeCellAdrs, &order.DeceasedAge, "Возраст"},
+		"deceased_height":        {DeceasedHeightCellAdrs, &order.DeceasedHeight, "Рост"},
+		"deceased_clothing_size": {DeceasedClothingSizeCellAdrs, &order.DeceasedClothingSize, "Размер одежды"},
+		"deceased_birth_date":    {DeceasedBirthDateCellAdrs, &order.DeceasedBirthDate, "Дата рождения"},
+		"deceased_death_date":    {DeceasedDeathDateCellAdrs, &order.DeceasedDeathDate, "Дата смерти"},
 	}
 
-	customerFullName, err := f.GetCellValue(OrderSheet, CustomerFullNameCellAdrs)
-	if err != nil {
-		fmt.Println("ERR: Не удалось получить значение ФИО заказчика")
-		return err
+	for key, cell := range cells {
+		value, err := f.GetCellValue(OrderSheet, cell.cellAddress)
+		if err != nil {
+			return fmt.Errorf("ERR: Не удалось прочитать поле %s(%s) [%s] - %w", key, cell.title, cell.cellAddress, err)
+		}
+		*cell.field = value
 	}
-
-	homePhone, err := f.GetCellValue(OrderSheet, HomePhomeCellAdrs)
-	if err != nil {
-		fmt.Println("ERR: Не удалось получить значение Добашний телефон")
-		return err
-	}
-
-	mobilePhone, err := f.GetCellValue(OrderSheet, MobilePhoneCellAdrs)
-	if err != nil {
-		fmt.Println("ERR: Не удалось получить значение Мобильный телефон")
-		return err
-	}
-
-	address, err := f.GetCellValue(OrderSheet, AddressCellAdrs)
-	if err != nil {
-		fmt.Println("ERR: Не удалось получить значение Адрес")
-		return err
-	}
-
-	deceasedFullName, err := f.GetCellValue(OrderSheet, DeceasedFullNameCellAdrs)
-	if err != nil {
-		fmt.Println("ERR: Не удалось получить значение ФИО умершего")
-		return err
-	}
-
-	deceasedAge, err := f.GetCellValue(OrderSheet, DeceasedAgeCellAdrs)
-	if err != nil {
-		fmt.Println("ERR: Не удалось получить значение Возраст умершего")
-		return err
-	}
-
-	deceasedHeight, err := f.GetCellValue(OrderSheet, DeceasedHeightCellAdrs)
-	if err != nil {
-		fmt.Println("ERR: Не удалось получить значение Рост умершего")
-		return err
-	}
-
-	deceasedClothingSize, err := f.GetCellValue(OrderSheet, DeceasedClothingSizeCellAdrs)
-	if err != nil {
-		fmt.Println("ERR: Не удалось получить значение Размер одежды умершего")
-		return err
-	}
-
-	deceasedBirthDate, err := f.GetCellValue(OrderSheet, DeceasedBirthDateCellAdrs)
-	if err != nil {
-		fmt.Println("ERR: Не удалось получить значение Дата рождения умершего")
-		return err
-	}
-
-	deceasedDeathDate, err := f.GetCellValue(OrderSheet, DeceasedDeathDateCellAdrs)
-	if err != nil {
-		fmt.Println("ERR: Не удалось получить значение Дата смерти умершего")
-		return err
-	}
-
-	fmt.Println("Номер закак: ", orderNumber)
-	fmt.Println("ФИО заказчика: ", customerFullName)
-	fmt.Println("Домашний телефон: ", homePhone)
-	fmt.Println("Мобильный телефон: ", mobilePhone)
-	fmt.Println("Адрес: ", address)
-	fmt.Println("ФИО умершего: ", deceasedFullName)
-	fmt.Println("Возраст умершего: ", deceasedAge)
-	fmt.Println("Рост умершего: ", deceasedHeight)
-	fmt.Println("Размер одежды умершего: ", deceasedClothingSize)
-	fmt.Println("Дата рождения умершего: ", deceasedBirthDate)
-	fmt.Println("Дата смерти умершего: ", deceasedDeathDate)
-
-	order.OrderNumber = orderNumber
-	order.CustomerFullName = customerFullName
-	order.HomePhome = homePhone
-	order.MobilePhone = mobilePhone
-	order.Address = address
-	order.DeceasedFullName = deceasedFullName
-	// order.DeceasedAge = deceasedAge  // TODO convert to int8
-	order.DeceasedHeight = deceasedHeight
-	order.DeceasedClothingSize = deceasedClothingSize
-	order.DeceasedBirthDate = deceasedBirthDate
-	order.DeceasedDeathDate = deceasedDeathDate
 
 	return nil
 }
